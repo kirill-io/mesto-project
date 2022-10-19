ca623784612918497ef95c8a8d4c0f61f45a33bb
Maxim Baryshnikov <baryshnikkov@gmail.com>
2022-10-19 10:57:53 +0300

    refactor: функциональность работы с card преобразована в класс Card

--- src/components/index.js
+++ src/components/index.js
@@ -1,90 +1,48 @@
 import '../pages/index.css';
 import * as constants from './constants.js';
-import {enableValidation, resetForm, clearFields} from './validate.js'
-import {createElement, deleteElementFormSubmitHandler, setLikeOnRender} from './card.js';
-import {openPopup, closePopup, fillInFormInputs, editProfileFormSubmitHandler, addPopupCloseHandlerOnClickOnOverlay, changeSaveButton, profileAvatarEditHandler, updateAvatarFormSubmitHandler} from './modal.js';
-import {checkPlaceName, saveData, fillInUserData, setAttribute} from './utils.js';
-import {getUserInformation, getInitialCards, addNewCard} from './Api.js';
+import Api from './Api.js';
+import Card from './Card.js';
 
-const buttonEditProfile = document.querySelector('.profile__edit');
-const buttonAddPlace = document.querySelector('.profile__add');
-const popupAdd = document.querySelector('#popupAdd');
-const popups = document.querySelectorAll('.popup');
-const buttonPopupDeleteImage = document.forms.deleteForm;
-const profileAvatar = document.querySelector('.profile__avatar');
+let idUser;
 
-const addNewElement = (placeName, placeLink) => {
-  const place = {};
-  place.name = checkPlaceName(placeName);
-  place.link = placeLink;
-  return place;
-};
+const api = new Api(constants.config);
 
-const renderNewElement = (objNewElement) => {
-  constants.elementsList.prepend(createElement(objNewElement, constants.userData));
-};
-
-const addElementFormSubmitHandler = (e) => {
-  e.preventDefault();
-  addNewCard(checkPlaceName(constants.inputPlace.value), constants.inputLink.value)
-    .then((result) => {
-      renderNewElement(addNewElement(constants.inputPlace.value, constants.inputLink.value));
-      setAttribute(constants.elementsList.firstElementChild, 'data-id', `${result._id}`);
-      setAttribute(constants.elementsList.firstElementChild.querySelector('.element__like'), 'data-id', `${result._id}`);
-      closePopup(constants.popupAdd);
-    })
-    .catch((err) => {
-      console.log(err);
-    })
-    .finally(() => {
-      changeSaveButton(constants.buttonPopupAddSave, false);
-    });
-  changeSaveButton(constants.buttonPopupAddSave, true);
-};
-
-Promise.all([getUserInformation(), getInitialCards()])
+Promise.all([api.getUserInformation(), api.getInitialCards()])
   .then(([userData, cards]) => {
-    saveData(userData, constants.userData);
-    fillInUserData(constants.userData, constants.profileName, constants.profileDesc, constants.profileAvatarImage);
-    cards.forEach((element) => {
-      constants.elementsList.append(createElement(element, constants.userData));
-      setLikeOnRender(element);
+    idUser = userData._id;
+
+    cards.forEach(el => {
+      const cardElement = new Card({
+          data: el,
+          idUser: idUser,
+          putLike: (el) => {
+            api.addLikeCard(el)
+              .then((res) => {
+                cardElement.like(res.likes);
+              })
+          },
+          deleteLike: (el) => {
+            api.deleteLikeCard(el)
+              .then((res) => {
+                cardElement.like(res.likes);
+              })
+          },
+          deleteCard: (el, cardId) => {
+            api.deleteCard(cardId)
+              .then(() => {
+                el.remove();
+              })
+          }
+        },'element');
+      const card = cardElement.renderer();
+      document.querySelector('.elements__list').append(card);
     });
   })
   .catch((err) => {
     console.log(err);
   });
 
-buttonEditProfile.addEventListener('click', () => {
-  fillInFormInputs(constants.profileName, constants.profileDesc, constants.inputName, constants.inputDesc);
-  resetForm (constants.formEditProfile, constants.buttonPopupEditSave, constants.validationConfig.inactiveButtonClass, constants.validationConfig.inputSelector, constants.validationConfig.inputErrorClass, constants.validationConfig.errorClass);
-  openPopup(constants.popupEdit);
-});
-
-popups.forEach((popup) => {
-  popup.addEventListener('mousedown', (evt) => {
-    if (evt.target.classList.contains('popup__close')) {
-      closePopup(popup);
-    }
-  });
-});
-
-constants.formEditProfile.addEventListener('submit', editProfileFormSubmitHandler);
-
-buttonAddPlace.addEventListener('click', () => {
-  clearFields(constants.formAddProfile);
-  resetForm (constants.formAddProfile, constants.buttonPopupAddSave, constants.validationConfig.inactiveButtonClass, constants.validationConfig.inputSelector, constants.validationConfig.inputErrorClass, constants.validationConfig.errorClass);
-  openPopup(popupAdd);
-});
-
-constants.formAddProfile.addEventListener('submit', addElementFormSubmitHandler);
-
-popups.forEach((popup) => addPopupCloseHandlerOnClickOnOverlay(popup));
-
-buttonPopupDeleteImage.addEventListener('submit', deleteElementFormSubmitHandler);
-
-profileAvatar.addEventListener('click', profileAvatarEditHandler);
-
-constants.formAvatarEdit.addEventListener('submit', updateAvatarFormSubmitHandler);
-
-enableValidation(constants.validationConfig);
+// api.addNewCard('forest', 'https://img.desktopwallpapers.ru/rocks/pics/wide/1920x1200/27640f370156a0e0ae3ee9608fc8480a.jpg')
+//   .then((res) => {
+//     console.log(res);
+//   })
