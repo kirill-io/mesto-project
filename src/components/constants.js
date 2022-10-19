b74fbee45eda9fbf60a0aa9ed8b0200568675319
Kirill-Begej <fleki679@yandex.ru>
2022-10-19 22:44:50 +0300

    refactor: функциональность validate.js преобразована в класс FormValidator

--- src/components/constants.js
+++ src/components/constants.js
@@ -4,4 +4,13 @@ export const config = {
     authorization: '901b5f54-a3c1-49a6-b698-d1aa30508c91',
     'Content-Type': 'application/json'
   }
-}
+};
+
+export const formAddProfile = document.forms.addForm;
+export const validationConfig = {
+  formSelector: '.popup__form',
+  inputSelector: '.popup__input',
+  inputErrorClass: 'popup__input_type_error',
+  errorClass: 'popup__input-error_active',
+  inactiveButtonClass: 'popup__save_inactive'
+};
